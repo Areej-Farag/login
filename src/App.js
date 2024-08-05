7f65309e066ad267000658093d31e28714481a24
Areej-Farag <rerefarag60@gmail.com>
2024-08-05 18:33:51 +0300

    project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
+import { CiUser } from "react-icons/ci";
+import { CiLock } from "react-icons/ci";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section >
+      <div className='rect'></div>
+      <div className='content-container'>
+      <div className=' form-content '>
+        <h2>Login</h2>
+        <form>
+          <p>Username</p>
+          <CiUser className='lock'/>
+          <input  type='text' placeholder='Type Your Username'></input>
+          <hr></hr>
+          <p>Password</p>
+          <CiLock className='lock' />
+          <input type='password'  placeholder=' Type Your Password'></input>
+          <hr></hr>
+          <p className='forget'><a href='#'>  Forget Your Password?</a></p>
+        </form>
+        <button> LOGIN </button>
+        <div className='form-text'>
+        <div className='icons-text'>
+          <p>Or Sign Up Using</p>
+          <div className='icons'>
+            <span className='icon facabook'><FaFacebookF /></span>
+            <span className='icon twitter'><FaTwitter  /></span>
+            <span className='icon google'> <FaGoogle  /></span>
+          </div>
+          </div>
+        
+          <div className='content-text'>
+          <p>Or Sign Up Using</p>
+          <p className='context-bold'>Sign Up</p>
+          </div>
+          </div>
+        
+
+
+      </div>
+      </div>
+    </section>
+ 
   );
 }
 
